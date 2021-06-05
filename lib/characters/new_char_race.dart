9ebdc449cea2789f56686cf6e53533b768683aff
David Leibovych <ariedov@gmail.com>
2021-06-05 11:38:29 +0300

    only uses `RacesBloc` for `NewCharRace` screen

--- lib/characters/new_char_race.dart
+++ lib/characters/new_char_race.dart
@@ -1,22 +1,26 @@
 import 'package:dnd_player_flutter/bloc/races/races_bloc.dart';
 import 'package:dnd_player_flutter/dto/race.dart';
+import 'package:dnd_player_flutter/repository/races_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NewCharRace extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<RacesBloc, RacesState>(
-        buildWhen: (oldState, newState) => newState is RacesLoaded,
-        builder: (context, state) => state is RacesEmpty ? SizedBox() : Column(
-          children: [
-            Expanded(
-              child: PageView.builder(itemBuilder: (context, index) {
-                return RaceCard(race: (state as RacesLoaded).races[index]);
-              }),
-            )
-          ],
+    return BlocProvider(
+      create: (context) => RacesBloc(RacesRepository())..add(LoadRaces()),
+      child: Scaffold(
+        body: BlocBuilder<RacesBloc, RacesState>(
+          buildWhen: (oldState, newState) => newState is RacesLoaded,
+          builder: (context, state) => state is RacesEmpty ? SizedBox() : Column(
+            children: [
+              Expanded(
+                child: PageView.builder(itemBuilder: (context, index) {
+                  return RaceCard(race: (state as RacesLoaded).races[index]);
+                }),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -31,6 +35,7 @@ class RaceCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Theme.of(context).primaryColorLight,
       child: Column(
         children: [
           Text(
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:dnd_player_flutter/bloc/races/races_bloc.dart';
 import 'package:dnd_player_flutter/characters/character_list.dart';
-import 'package:dnd_player_flutter/repository/races_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,51 +9,45 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => RacesBloc(RacesRepository())..add(LoadRaces()),
-          )
-        ],
-        child: MaterialApp(
-          title: 'DnD',
-          theme: ThemeData(
-            primaryColor: Color(0xFF1A1E21),
-            primaryColorLight: Color(0xFF272E32),
-            accentColor: Color(0xFFFF5251),
-            backgroundColor: Color(0xFF1A1E21),
-            scaffoldBackgroundColor: Color(0xFF1A1E21),
-            textTheme: TextTheme(
-              headline1: TextStyle(
-                // page titles
-                fontSize: 32,
-                color: Color(0xFFE5E1DE),
-              ),
-              headline2: TextStyle(
-                  // bigger headlines in the root of views
-                  fontSize: 24,
-                  color: Color(0xFFDCDAD9)),
-              headline5: TextStyle(
-                  // section titles
-                  fontSize: 18,
-                  color: Color(0xFFDCDAD9)),
-              subtitle1: TextStyle(
-                  // subtitles
-                  fontSize: 12,
-                  color: Color(0xFF9D9D9D)),
-              bodyText1: TextStyle(
-                // small titles
-                fontSize: 12,
-                color: Color(0x99DCDAD9),
-              ),
-              bodyText2: TextStyle(
-                // regular text
-                fontSize: 12,
-                color: Color(0xFFDCDAD9),
-              ),
-            ),
+    return MaterialApp(
+      title: 'DnD',
+      theme: ThemeData(
+        primaryColor: Color(0xFF1A1E21),
+        primaryColorLight: Color(0xFF272E32),
+        accentColor: Color(0xFFFF5251),
+        backgroundColor: Color(0xFF1A1E21),
+        scaffoldBackgroundColor: Color(0xFF1A1E21),
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            // page titles
+            fontSize: 32,
+            color: Color(0xFFE5E1DE),
           ),
-          home: CharacterList(),
-        ));
+          headline2: TextStyle(
+              // bigger headlines in the root of views
+              fontSize: 24,
+              color: Color(0xFFDCDAD9)),
+          headline5: TextStyle(
+              // section titles
+              fontSize: 18,
+              color: Color(0xFFDCDAD9)),
+          subtitle1: TextStyle(
+              // subtitles
+              fontSize: 12,
+              color: Color(0xFF9D9D9D)),
+          bodyText1: TextStyle(
+            // small titles
+            fontSize: 12,
+            color: Color(0x99DCDAD9),
+          ),
+          bodyText2: TextStyle(
+            // regular text
+            fontSize: 12,
+            color: Color(0xFFDCDAD9),
+          ),
+        ),
+      ),
+      home: CharacterList(),
+    );
   }
 }
