a323767eb630ebac0955170c8c2395a7df699839
David <ariedov@gmail.com>
2021-11-17 23:38:52 +0200

    uses tabs instead of indicators

--- lib/ui/characters/pager_with_indicators.dart
+++ lib/ui/characters/pager_with_indicators.dart
@@ -14,67 +14,42 @@ class PagerWithIndicators extends StatefulWidget {
 
 class PagerWithIndicatorsState extends State<PagerWithIndicators> {
   late PageController _controller;
-  late PageController _indicatorController;
 
   @override
   void initState() {
     super.initState();
-    _controller = PageController()
-      ..addListener(
-        () => setState(() {
-          _indicatorController.jumpTo(_controller.offset * 0.3);
-        }),
-      );
-
-    _indicatorController = PageController(viewportFraction: 0.3);
+    _controller = PageController();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          height: 100,
-          child: PageView(
-            controller: _indicatorController,
-            padEnds: false,
-            children: widget.namedPages.keys
-                .map((title) => IndicatorItem(title: title, opacity: 1.0))
-                .toList(),
-          ),
-        ),
-        Expanded(
-          child: PageView(
-            children: widget.namedPages.values
-                .map((e) => Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: e,
-                    ))
-                .toList(),
-            controller: _controller,
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class IndicatorItem extends StatelessWidget {
-  final String title;
-  final double opacity;
-
-  const IndicatorItem({
-    Key? key,
-    required this.title,
-    required this.opacity,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: TextStyle(color: Color(0xFFE5E1DE)),
+    return DefaultTabController(
+      length: widget.namedPages.length,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TabBar(
+              indicatorColor: Colors.red,
+              indicatorPadding: EdgeInsets.symmetric(horizontal: 32),
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              isScrollable: true,
+              unselectedLabelColor: Colors.white.withOpacity(0.3),
+              tabs: widget.namedPages.keys
+                  .map((e) => Tab(
+                        child: Text(e),
+                      ))
+                  .toList()),
+          Expanded(
+            child: TabBarView(
+                children: widget.namedPages.values
+                    .map((e) => Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 16),
+                          child: e,
+                        ))
+                    .toList()),
+          )
+        ],
+      ),
     );
   }
 }
