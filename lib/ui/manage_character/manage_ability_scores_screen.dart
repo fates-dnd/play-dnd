ee5dc3d124dc4287921899d18f8c620d2a733f28
David Leibovych <david.leibovych@anonybit.io>
2022-08-05 23:38:02 +0300

    displays ability scores to change

---
+++ lib/ui/manage_character/manage_ability_scores_screen.dart
@@ -0,0 +1,82 @@
+import 'package:dnd_player_flutter/bloc/character/character_bloc.dart';
+import 'package:dnd_player_flutter/data/characteristics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class ManageAbilityScoresScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context)!;
+    return BlocBuilder<CharacterBloc, CharacterState>(
+      builder: (context, state) => Scaffold(
+        appBar: AppBar(
+          title: Text(localizations.manage_ability_scores),
+        ),
+        body: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            children: Characteristic.values
+                .map((characteristic) => _CharacteristicRow(
+                      characteristic: characteristic,
+                      value: _getCharacteristicStat(characteristic, state),
+                      bonus: 0,
+                    ))
+                .toList()),
+      ),
+    );
+  }
+
+  int _getCharacteristicStat(
+      Characteristic characteristic, CharacterState state) {
+    switch (characteristic) {
+      case Characteristic.STRENGTH:
+        return state.strength;
+      case Characteristic.DEXTERITY:
+        return state.dexterity;
+      case Characteristic.CONSTITUTION:
+        return state.constitution;
+      case Characteristic.INTELLIGENCE:
+        return state.intelligence;
+      case Characteristic.WISDOM:
+        return state.wisdom;
+      case Characteristic.CHARISMA:
+        return state.charisma;
+    }
+  }
+}
+
+class _CharacteristicRow extends StatelessWidget {
+  final Characteristic characteristic;
+  final int value;
+  final int bonus;
+
+  const _CharacteristicRow({
+    Key? key,
+    required this.characteristic,
+    required this.value,
+    required this.bonus,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Expanded(
+                child: Text(
+              characteristic.getName(context),
+              style: TextStyle(fontSize: 18),
+            )),
+            Text(
+              value.toString(),
+              style: TextStyle(fontSize: 18),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/manage_character/manage_character_screen.dart
+++ lib/ui/manage_character/manage_character_screen.dart
@@ -1,5 +1,6 @@
 import 'package:dnd_player_flutter/bloc/character/character_bloc.dart';
 import 'package:dnd_player_flutter/bloc/character_list/character_list_bloc.dart';
+import 'package:dnd_player_flutter/ui/manage_character/manage_ability_scores_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -17,7 +18,20 @@ class ManageCharacterScreen extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 16),
           child: Column(
             children: [
-              Expanded(child: SizedBox()),
+              Expanded(
+                  child: ListView(
+                children: [
+                  _ManageMenuItem(
+                    title: localizations.manage_ability_scores,
+                    onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ManageAbilityScoresScreen())),
+                  ),
+                  _ManageMenuItem(
+                    title: localizations.manage_proficiencies,
+                    onTap: () {},
+                  ),
+                ],
+              )),
               Center(
                 child: ElevatedButton(
                   onPressed: () {
@@ -37,6 +51,43 @@ class ManageCharacterScreen extends StatelessWidget {
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ManageMenuItem extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+
+  const _ManageMenuItem({
+    Key? key,
+    required this.title,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+        child: Row(
+          children: [
+            Expanded(
+                child: Text(
+              title,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            )),
+            Icon(
+              Icons.chevron_right,
+              color: Colors.white,
+            ),
+          ],
         ),
       ),
     );
